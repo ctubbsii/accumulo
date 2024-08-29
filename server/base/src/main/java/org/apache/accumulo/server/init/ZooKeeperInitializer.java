fee60b94327f65cdf380a6ba993b0e54c7beede9
meatballspaghetti <69256191+meatballspaghetti@users.noreply.github.com>
2024-08-29 19:52:21 +0000

    WIP. Couldn't figure out yet how to test code

--- server/base/src/main/java/org/apache/accumulo/server/init/ZooKeeperInitializer.java
+++ server/base/src/main/java/org/apache/accumulo/server/init/ZooKeeperInitializer.java
@@ -25,6 +25,7 @@ import java.io.IOException;
 import org.apache.accumulo.core.Constants;
 import org.apache.accumulo.core.client.admin.TimeType;
 import org.apache.accumulo.core.clientImpl.Namespace;
+import org.apache.accumulo.core.clientImpl.ZooKeeperMapping;
 import org.apache.accumulo.core.data.InstanceId;
 import org.apache.accumulo.core.data.Mutation;
 import org.apache.accumulo.core.data.Value;
@@ -117,6 +118,9 @@ public class ZooKeeperInitializer {
         ZooUtil.NodeExistsPolicy.FAIL);
     TableManager.prepareNewNamespaceState(context, Namespace.ACCUMULO.id(),
         Namespace.ACCUMULO.name(), ZooUtil.NodeExistsPolicy.FAIL);
+
+    ZooKeeperMapping.initializeNamespaceMap(context.getZooReaderWriter(),
+            zkInstanceRoot + Constants.ZNAMESPACES);
 
     TableManager.prepareNewTableState(context, AccumuloTable.ROOT.tableId(),
         Namespace.ACCUMULO.id(), AccumuloTable.ROOT.tableName(), TableState.ONLINE,
